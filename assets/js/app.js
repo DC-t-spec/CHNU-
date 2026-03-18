744835dae971011b406d5302cea434e773efffc7
DC-t-spec <dcnet025@gmail.com>
2026-03-18 03:56:27 +0000

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,31 @@
 console.log("CHNU app loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("CHNU iniciado");
+
+  const app = document.getElementById("app");
+
+  const currentPage = Router.getCurrentPage();
+
+  console.log("Página atual:", currentPage);
+
+  renderPage(app, currentPage);
+});
+
+function renderPage(app, page) {
+  switch (page) {
+    case "dashboard":
+      app.innerHTML = "<h1>Dashboard CHNU</h1>";
+      break;
+
+    case "documents":
+      app.innerHTML = "<h1>Documents CHNU</h1>";
+      break;
+
+    case "inventory":
+      app.innerHTML = "<h1>Inventory CHNU</h1>";
+      break;
+
+    default:
+      app.innerHTML = "<h1>CHNU</h1>";
+  }
+}
