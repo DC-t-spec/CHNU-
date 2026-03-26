c3978c07d45aea9f8ad9b6f3412dfb58c163fc0f
DC-t-spec <dcnet025@gmail.com>
2026-03-27 01:23:23 +0200

    Update document-posting.js

--- assets/js/modules/documents/document-posting.js
+++ assets/js/modules/documents/document-posting.js
@@ -1,8 +1,25 @@
 import { postDocument } from '../../core/state.js';
 
-export function handleDocumentPosting(documentId) {
+export function handleDocumentPosting(documentId, options = {}) {
+  const {
+    redirectTo = 'detail',
+    onSuccess = null,
+  } = options;
+
   try {
     postDocument(documentId);
+
+    if (typeof onSuccess === 'function') {
+      onSuccess();
+      return;
+    }
+
+    if (redirectTo === 'list') {
+      window.location.hash = window.location.hash || '#documents';
+      window.dispatchEvent(new HashChangeEvent('hashchange'));
+      return;
+    }
+
     window.location.hash = `#documents/view?id=${documentId}`;
   } catch (error) {
     window.alert(error.message || 'Não foi possível postar o documento.');
