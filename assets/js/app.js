a92e4a255928ebe4c8d1b71e61e15ff1f8966674
DC-t-spec <dcnet025@gmail.com>
2026-03-31 22:42:20 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,8 @@ import { renderDashboardPage } from './modules/dashboard/dashboard.page.js';
 import { renderDocumentsListPage } from './modules/documents/documents-list.page.js';
 import { renderDocumentDetailPage } from './modules/documents/document-detail.page.js';
 import { renderDocumentFormPage } from './modules/documents/document-form.page.js';
+import { renderInventoryBalancesPage } from './modules/inventory/inventory-balances.page.js';
+import { renderInventoryLedgerPage } from './modules/inventory/inventory-ledger.page.js';
 
 function bootstrapRoutes() {
   registerRoute('/dashboard', renderDashboardPage);
@@ -11,6 +13,10 @@ function bootstrapRoutes() {
   registerRoute('/documents/new', renderDocumentFormPage);
   registerRoute('/documents/view', renderDocumentDetailPage);
   registerRoute('/documents/edit', renderDocumentFormPage);
+
+  registerRoute('/inventory', renderInventoryBalancesPage);
+  registerRoute('/inventory-balances', renderInventoryBalancesPage);
+  registerRoute('/inventory-ledger', renderInventoryLedgerPage);
 }
 
 function bootstrapApp() {
