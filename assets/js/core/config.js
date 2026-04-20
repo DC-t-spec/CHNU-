2d1894dd30fd8b58006e189152b2730107fd0642
DC-t-spec <dcnet025@gmail.com>
2026-04-20 14:38:15 +0100

    Update config.js

--- assets/js/core/config.js
+++ assets/js/core/config.js
@@ -1,5 +1,5 @@
 // assets/js/core/config.js
 
-export const SUPABASE_URL = '';
-export const SUPABASE_ANON_KEY = '';
-export const SUPABASE_ENABLED = false;
+export const SUPABASE_URL = 'https://jnovwijxuplrcjbtnerc.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impub3Z3aWp4dXBscmNqYnRuZXJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA5MTI0MjcsImV4cCI6MjA4NjQ4ODQyN30.99oSvJ94iq4IfukLQTCFi5DvdAPbooDV4Ll9KgAuGD0';
+export const SUPABASE_ENABLED = true;
